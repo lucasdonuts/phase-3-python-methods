0b0506836ceade06a992b439def11f90cee5346f
Lucas Duncan <lucasdonuts@gmail.com>
2022-12-20 18:00:08 -0600

    All tests passing

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number / 2
+
+# function greet_programmer() prints "Hello, programmer!" F
+# function greet() prints "Hello, {name}!" F
+# function greet_with_default() prints "Hello, programmer!" F
+# function greet_with_default() prints "Hello, {name}!" F
+# function add() calculates 45 + 55 = 100 F
+# function halve() halves integer input F
+# function halve() halves float input F
